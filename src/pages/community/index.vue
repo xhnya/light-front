468dd55d3b7ddc74920bcc1cf0d88a42e526d99c
xhn <930957853@qq.com>
2022-01-11 17:40:43 +0800

    社区

--- src/pages/community/index.vue
+++ src/pages/community/index.vue
@@ -1,6 +1,46 @@
 <template>
-  <div>
-    community
+  <div class="community-first">
+    <el-row>
+      <el-col :span="5">
+        <div class="grid-content bg-purple">
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span>卡片名称</span>
+              <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+            </div>
+            <div v-for="o in 4" :key="o" class="text item">
+              {{ '列表内容 ' + o }}
+            </div>
+          </el-card>
+        </div>
+      </el-col>
+      <el-col :span="8">
+        <div class="grid-content bg-purple-light">
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span>卡片名称</span>
+              <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+            </div>
+            <div v-for="o in 4" :key="o" class="text item">
+              {{ '列表内容 ' + o }}
+            </div>
+          </el-card>
+        </div>
+      </el-col>
+      <el-col :span="8">
+        <div class="grid-content bg-purple">
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span>卡片名称</span>
+              <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+            </div>
+            <div v-for="o in 4" :key="o" class="text item">
+              {{ '列表内容 ' + o }}
+            </div>
+          </el-card>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -14,5 +54,9 @@ export default {
 </script>
 
 <style scoped>
-
+.community-first {
+  margin-left: 10%;
+  margin-right: 10%;
+  margin-top: 30px;
+}
 </style>
